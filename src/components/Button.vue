86cc48ec6997cd99c16d6944d645155a1a6a2197
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-09 17:09:40 -0300

    Buttons event, Tasks components, static state & dynamic class

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,11 @@
 <template>
-  <button :style="{ background: color }" class="btn">{{ text }}</button>
+  <button
+    :style="{ background: color }"
+    class="btn"
+    @click="onClick()"
+  >
+    {{ text }}
+  </button>
 </template>
 
 <script>
@@ -8,6 +14,11 @@ export default {
   props: {
     text: String,
     color: String,
+  },
+  methods: {
+    onClick() {
+      console.log('click');
+    },
   },
 };
 </script>
