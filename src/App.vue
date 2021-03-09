0516c953d049b18733aee34d5046568b31fce3aa
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-09 17:25:07 -0300

    Using emit events with delete task

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
 
@@ -19,6 +19,16 @@ export default {
     return {
       tasks: [],
     };
+  },
+  methods: {
+    deleteTask(id) {
+      // eslint-disable-next-line no-restricted-globals
+      const confirmation = confirm('Are you sure?');
+      console.log(confirmation);
+      if (confirmation) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
   },
   created() {
     this.tasks = [
