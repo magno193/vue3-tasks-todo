e1da90ec2d051ad7899e679cabccc355d83d326f
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-10 01:23:02 -0300

    Toggle button open/close task form complete

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button text="Add Task" color="green"/>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
   </header>
 </template>
 
@@ -15,10 +19,12 @@ export default {
       type: String,
       default: 'Header component default value',
     },
+    showAddTask: Boolean,
   },
   components: {
     Button,
   },
+  emits: ['toggle-add-task'],
 };
 </script>
 
